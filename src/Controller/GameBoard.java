8dae8f345d89e645e89100aac83ca212e7db846a
0205frwarens <0205frwarens@PC9844.domepfc.net>
2018-02-06 21:37:15 +0100

    Début de squelette

---
+++ src/Controller/GameBoard.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import java.util.Observable;
+
+
+/**
+ *
+ * @author 0205frwarens
+ */
+public class GameBoard extends Observable {
+private int LIGNES  = 5;
+private int COLONNES  = 5;
+Case mer[][] = new Case[LIGNES][COLONNES];
+
+    public GameBoard() {
+    }
+
+
+
+}
